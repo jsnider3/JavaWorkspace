731525cbe93aaaab3bcf573d259a0e67a3ec8768
Joshua Snider <jsnider3@gmu.edu>
2015-02-22 15:57:00 -0500

    camelCase and nthPrime

--- Euler/Euler.py
+++ Euler/Euler.py
@@ -1,19 +1,38 @@
-def SumSquares(aNum):
+def sumSquares(aNum):
   tSum = 0
   for tCount in xrange(1, aNum + 1) :
     tSum = tSum + (tCount ** 2)
   return tSum
 
-def SquareSum(aNum):
+def squareSum(aNum):
   tSum = 0
   for tCount in xrange(1, aNum + 1):
     tSum = tSum + tCount
   return tSum ** 2
 
-def PrimeFactors(aInt):
+def primeFactors(aInt):
   for tCount in xrange(2, aInt + 1):
     if aInt % tCount == 0:
-      return [tCount] + PrimeFactors(aInt / tCount)    
+      return [tCount] + primeFactors(aInt / tCount)    
   return []
 
-print(max(PrimeFactors(600851475143)))
+def isPrime(aInt):
+  for tCount in xrange(2, int(aInt ** (.5) + 1)):
+    if aInt % tCount == 0:
+      return False
+  return True
+
+def nthPrime(aNth):
+  assert(aNth > 0)
+  tPrime = 1
+  tCount = 2
+  tFound = 0
+  while tFound < aNth:
+    if isPrime(tCount):
+      tPrime = tCount
+      tFound = tFound + 1
+    tCount = tCount + 1
+  return tPrime
+  
+
+print(nthPrime(10001))
