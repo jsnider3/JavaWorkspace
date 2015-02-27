7622c53bcbe818c665c079ba36eaad163585d3a9
Joshua Snider <jsnider3@gmu.edu>
2015-02-26 22:09:00 -0500

    Spelling mistake

--- Euler/euler.py
+++ Euler/euler.py
@@ -281,7 +281,7 @@ def british_number_string(aNum):
            "5" : "fifty", "6" : "sixty", "7" : "seventy", "8" : "eighty",
            "9" : "ninety", "11" : "eleven", "12" : "twelve", "13" : "thirteen",
            "14" : "fourteen", "15" : "fifteen", "16" : "sixteen", 
-           "17" : "seventeen", "18" : "eighteen", "19" : "ninteen"}
+           "17" : "seventeen", "18" : "eighteen", "19" : "nineteen"}
   tOnes = {"1" : "one", "2" : "two", "3" : "three", "4" : "four",
            "5" : "five", "6" : "six", "7" : "seven", "8" : "eight",
            "9" : "nine"}
@@ -600,7 +600,6 @@ def triangle_max_path(aTriangle):
   return tMax[0]
 
 def largest_grid_product(aGrid):
-  #FIXME: That's not how lists of lists work in python.
   tRows = len(aGrid)
   tMaxProduct = -float("inf")
   for tY in range(tRows):
@@ -627,6 +626,7 @@ def largest_grid_product(aGrid):
 
 def main():
   print("REDACTED")
+  print(sum(map(len, map(british_number_string, range(1, 1001)))))
 
 if __name__ == "__main__":
   main()
