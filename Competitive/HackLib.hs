73c12990c2291f836dc045cb54e92671a08a0439
Joshua Snider <jsnider3@gmu.edu>
2015-03-15 21:40:22 -0400

    filter_less_than

--- Competitive/HackLib.hs
+++ Competitive/HackLib.hs
@@ -4,4 +4,12 @@
 -}
 
 arr_repl :: Int -> [Int] -> [Int]
-arr_repl n arr = concat(map (replicate n) arr)-- Complete this function
+arr_repl n arr = concat(map (replicate n) arr)
+
+{-
+  filter_less_than - Return the elements of arr less than n.
+-}
+filter_less_than :: Int -> [Int] -> [Int]
+filter_less_than n [] = []
+filter_less_than n arr = (if arr !! 0 < n then [arr !! 0] else [])
+                           ++ filter_less_than n ( tail arr)
