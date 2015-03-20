91513810cd6c34ac30f192d79138565769c66919
Joshua Snider <jsnider3@gmu.edu>
2015-03-19 23:08:37 -0400

    Still buggy, still slow

--- Competitive/Haskell/Kyle.hs
+++ Competitive/Haskell/Kyle.hs
@@ -1,22 +1,23 @@
 import Data.Bits
+import Data.Maybe
 import Debug.Trace
 
 highOrder :: Int -> Int
 highOrder n = getHighOrder n 0
 
 getHighOrder :: Int -> Int -> Int
-getHighOrder n shift = case (shift, testBit n  (63 - shift))) of
+getHighOrder n shift = case (shift, testBit n  (63 - shift)) of
                         (64, _) -> (-1)
-                        (_, 0) -> getHighOrder n (shift + 1)
+                        (_, False) -> getHighOrder n (shift + 1)
                         _   -> (63 - shift)
 
 lowOrder :: Int -> Int
 lowOrder n = getLowOrder n 0
 
 getLowOrder :: Int -> Int -> Int
-getLowOrder n shift = case (shift, testBit n  shift) of
+getLowOrder n shift = case (shift, testBit n shift) of
                         (64, _) -> (-1)
-                        (_, 0) -> getLowOrder n (shift + 1)
+                        (_, False) -> getLowOrder n (shift + 1)
                         _   -> (shift)
 
 winning :: Int -> Bool
@@ -30,11 +31,18 @@ children :: Int -> [Int]
 children pos = concat [knockdownOnes pos, knockdownTwos pos]
 
 knockdownOnes :: Int -> [Int]
-knockdownOnes n = concat(map (\x -> if testBit n x then [clearBit n x] else []) [0..10])
+knockdownOnes n = catMaybes(map (knockdownOne n) [0..10])
+
+knockdownOne n x = case testBit n x of
+                        True -> Just (clearBit n x)
+                        False -> Nothing
 
 knockdownTwos :: Int -> [Int]
-knockdownTwos n = concat(map (\x -> if and[testBit n x, testBit n (x-1)] then [clearBit (n-1) (clearBit n x)] else []) [0..10])
+knockdownTwos n = catMaybes(map (\x -> if and[testBit n x, testBit n (x-1)] then Just (clearBit (n-1) (clearBit n x)) else Nothing) [0..10])
 
+knockdownTwo n x = case and [testBit n x, testBit n (x-1)] of
+                        True -> Just (clearBit (clearBit n x) x-1)
+                        False -> Nothing
 binaryListToInt :: [Int] -> Int
 binaryListToInt = foldl1 (\ a b -> 2*a + b) 
 
