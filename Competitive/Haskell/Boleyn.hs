b948ff04aa5dc66b61ae54d87d40ceac17c069c6
Joshua Snider <jsnider3@gmu.edu>
2015-03-27 02:41:23 -0400

    More Boleyn

--- Competitive/Haskell/Boleyn.hs
+++ Competitive/Haskell/Boleyn.hs
@@ -1,17 +1,21 @@
 import qualified Data.Map as Map
 import System.IO.Unsafe
 
-create_hierarchy :: Int -> IO (Map.Map Int Int)
+create_hierarchy :: Int -> IO (Map.Map Int [Int])
 create_hierarchy n = do
   links <- mapM (const getLine) [1..n - 1]
   let supes = map makeIntList (map words links) in do
-    return $ Map.fromList(zip (map head supes)(map (\x -> x !! 1) supes))
+    return $ edgesToTree(zip (map head supes)(map (\x -> x !! 1) supes))
+
+edgesToTree :: [(Int,Int)] -> (Map.Map Int [Int])
+edgesToTree [] = Map.empty
+edgesToTree ((a,b):rest) = Map.insertWith (++) b [a] (edgesToTree rest)
 
 load_salaries :: IO (Map.Map Int Int)
 load_salaries = do
   line <- getLine
   let dat = makeIntList (words line) in
-    let sals = Map.fromList(zip [1,2..] dat) in
+    let sals = Map.fromList(zip [1, 2..] dat) in
       return sals
 
 makeIntList :: [String] -> [Int]
