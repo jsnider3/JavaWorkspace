47e5af3bcccb8a2db885f5b6a060b1ed792ace2d
Joshua Snider <jsnider3@gmu.edu>
2015-03-27 02:19:19 -0400

    Start of Boleyn

---
+++ Competitive/Haskell/Boleyn.hs
@@ -0,0 +1,26 @@
+import qualified Data.Map as Map
+import System.IO.Unsafe
+
+create_hierarchy :: Int -> IO (Map.Map Int Int)
+create_hierarchy n = do
+  links <- mapM (const getLine) [1..n - 1]
+  let supes = map makeIntList (map words links) in do
+    return $ Map.fromList(zip (map head supes)(map (\x -> x !! 1) supes))
+
+load_salaries :: IO (Map.Map Int Int)
+load_salaries = do
+  line <- getLine
+  let dat = makeIntList (words line) in
+    let sals = Map.fromList(zip [1,2..] dat) in
+      return sals
+
+makeIntList :: [String] -> [Int]
+makeIntList = map (\x -> read x::Int)
+
+main :: IO ()
+main = do
+  nums <- getLine
+  tree <- create_hierarchy (read(head(words nums))::Int)
+  print tree
+  sals <- load_salaries
+  print sals
