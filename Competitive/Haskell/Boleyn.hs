095013354d42f1928c3c6cc3919d1e4ebf6d25a3
Joshua Snider <jsnider3@gmu.edu>
2015-03-27 18:42:30 -0400

    Halfway done

--- Competitive/Haskell/Boleyn.hs
+++ Competitive/Haskell/Boleyn.hs
@@ -1,18 +1,24 @@
 import qualified Data.Map as Map
+import Data.Maybe
+import Data.List
 import System.IO.Unsafe
 
-create_hierarchy :: Int -> IO (Map.Map Int [Int])
-create_hierarchy n = do
+createHierarchy :: Int -> IO (Map.Map Int [Int])
+createHierarchy n = do
   links <- mapM (const getLine) [1..n - 1]
   let supes = map makeIntList (map words links) in do
     return $ edgesToTree(zip (map head supes)(map (\x -> x !! 1) supes))
 
-edgesToTree :: [(a, a)] -> (Map.Map a [a])
-edgesToTree [] = Map.empty
-edgesToTree ((a,b):rest) = Map.insertWith (++) b [a] (edgesToTree rest)
+descendents tree root = case (Map.lookup root tree) of
+                          (Just a) -> a ++ concat(map (descendents tree) a)
+                          _ -> []
 
-load_salaries :: IO (Map.Map Int Int)
-load_salaries = do
+edgesToTree :: (Ord a) => [(a, a)] -> (Map.Map a [a])
+edgesToTree [] = Map.empty
+edgesToTree ((a, b):rest) = Map.insertWith (++) b [a] (edgesToTree rest)
+
+loadSalaries :: IO (Map.Map Int Int)
+loadSalaries = do
   line <- getLine
   let dat = makeIntList (words line) in
     let sals = Map.fromList(zip [1, 2..] dat) in
@@ -21,10 +27,25 @@ load_salaries = do
 makeIntList :: [String] -> [Int]
 makeIntList = map (\x -> read x::Int)
 
+processQueries :: Map.Map Int [Int] -> Map.Map Int Int -> [[Int]] -> [Int]
+procesQueries _ _ [] = []
+processQueries tree sals ([a, b]:rest) = []
+  
+
+queries :: Map.Map Int [Int] -> Map.Map Int Int -> Int -> IO ()
+queries tree sals n = do
+  print tree
+  print sals
+  stuff <- mapM (const getLine) [1..n - 1]
+  let input = map makeIntList (map words stuff) in do
+    print $ processQueries tree sals input
+
+sortByMap vals keys = sortBy (\x y -> compare (Map.lookup x vals)(Map.lookup x vals)) keys
+
 main :: IO ()
 main = do
   nums <- getLine
-  tree <- create_hierarchy (read(head(words nums))::Int)
-  print tree
-  sals <- load_salaries
-  print sals
+  tree <- createHierarchy (read(head(words nums))::Int)
+  sals <- loadSalaries
+  queries tree sals (read((words nums) !! 1)::Int)
+
