4b760c6bcfa0fdb3e5202021c6740786506b6dc6
Joshua Snider <jsnider3@gmu.edu>
2015-03-27 21:26:23 -0400

    Changed Map -> Array, still too slow.

--- Competitive/Haskell/Boleyn.hs
+++ Competitive/Haskell/Boleyn.hs
@@ -1,47 +1,62 @@
+import Data.Array.IArray
 import qualified Data.Map as Map
 import Data.Maybe
 import Data.List
 import System.IO.Unsafe
 
-createHierarchy :: Int -> IO (Map.Map Int [Int])
+{-
+  Slowest test case takes me 30 times the cutoff to do.
+  This almost certainly means that I need a better data
+  structure.
+  On the plus side, the results are 100% correct.
+-}
+
+createHierarchy :: Int -> IO (Array Int [Int])
 createHierarchy n = do
   links <- mapM (const getLine) [1..n - 1]
   let supes = map makeIntList (map words links) in do
-    return $ edgesToTree(zip (map head supes)(map (\x -> x !! 1) supes))
+    return $ treeToArray n $ edgesToTree(zip (map head supes)(map (\x -> x !! 1) supes))
 
-descendents tree root = case (Map.lookup root tree) of
-                          (Just a) -> a ++ concat(map (descendents tree) a)
-                          _ -> []
+descendents tree root = case (tree ! root ) of
+                          [] -> []
+                          a -> a ++ concat(map (descendents tree) a)
 
 edgesToTree :: (Ord a) => [(a, a)] -> (Map.Map a [a])
 edgesToTree [] = Map.empty
 edgesToTree ((a, b):rest) = Map.insertWith (++) b [a] (edgesToTree rest)
 
-loadSalaries :: IO (Map.Map Int Int)
+treeToArray :: Int -> (Map.Map Int [Int]) -> (Array Int [Int])
+treeToArray n a = array (1, n)(Map.assocs a ++ zip (filter (\x -> Map.notMember x a)[1..n])(repeat []))
+
+loadSalaries :: IO (Array Int Int)
 loadSalaries = do
   line <- getLine
   let dat = makeIntList (words line) in
-    let sals = Map.fromList(zip [1, 2..] dat) in
+    let sals = array (1, length dat)(zip [1,2..] dat) in do
       return sals
 
 makeIntList :: [String] -> [Int]
 makeIntList = map (\x -> read x::Int)
 
-processQueries :: Map.Map Int [Int] -> Map.Map Int Int -> [[Int]] -> [Int]
-processQueries tree sals [[a, b]] = [sortByMap sals (descendents tree a) !! (b - 1)]
+processQueries :: Array Int [Int] -> Array Int Int -> [[Int]] -> [Int]
+processQueries tree sals [[a, b]] = [quickSelectWithFunc (\x -> sals ! x) (descendents tree a) (b - 1)]
 processQueries tree sals ([a, b]:[x,y]:rest) = 
-  let hd = sortByMap sals (descendents tree a) !! (b - 1) in
-    hd:(processQueries tree sals ([hd+x,y]:rest))
+  let hd = quickSelectWithFunc (\x -> sals ! x) (descendents tree a) (b - 1) in
+    hd:(processQueries tree sals ([hd + x, y]:rest))
 
-printQueries :: Map.Map Int [Int] -> Map.Map Int Int -> Int -> IO [()]
+printQueries :: Array Int [Int] -> Array Int Int -> Int -> IO [()]
 printQueries tree sals n = do
---  print tree
---  print sals
   stuff <- mapM (const getLine) [1..n]
   let input = map makeIntList (map words stuff) in do
     mapM print $ processQueries tree sals input
 
-sortByMap vals keys = sortBy (\x y -> compare (fromJust $Map.lookup x vals)(fromJust $Map.lookup y vals)) keys
+quickSelectWithFunc :: (Ord b) => (a-> b) -> [a] -> Int -> a
+quickSelectWithFunc foo (x:xs) ind 
+                     | ind < l     = quickSelectWithFunc foo ys ind
+                     | ind > l     = quickSelectWithFunc foo zs (ind - l - 1)
+                     | otherwise = x
+  where (ys, zs) = partition (\n -> foo n < foo x) xs
+        l = length ys
 
 main :: IO [()]
 main = do
