57ec55224afd97cbc157951151df445089c7f1a1
Joshua Snider <jsnider3@masonlive.gmu.edu>
2015-05-31 21:13:40 -0400

    Princess Bot thing

---
+++ Competitive/OCaml/PrincessBot/princessbot.ml
@@ -0,0 +1,38 @@
+open Core.Std
+open Printf
+open Scanf
+
+(** 
+  Needs work.
+*)
+
+let first_exn o = match o with
+  Some (i, c) -> i
+  |None -> invalid_arg "You promised me this wouldn't happen.";;
+
+let rec grid_locs rows n p r =
+  match rows with
+    0 -> (p, r)
+    |_ -> let ln = String.to_list (read_line ()) in
+      let p_search = List.findi ln ~f:(fun a b -> b = 'p') in
+      let r_search = List.findi ln ~f:(fun a b -> b = 'm') in
+      let new_p = if p_search = None then p else (n, first_exn p_search) in
+      let new_r = if r_search = None then r else (n, first_exn r_search) in
+      grid_locs (rows - 1) (n + 1) new_p new_r;;
+
+let rec dotimes f n = match n with
+  0 -> ()
+  |_ -> f (); dotimes f (n - 1);;
+
+let print_updown n = if n > 0 then dotimes (fun a -> printf "DOWN\n") n
+  else if n < 0 then dotimes (fun a -> printf "UP\n") (-n);;
+
+let print_leftright n = if n > 0 then dotimes (fun a ->printf "LEFT\n") n
+  else if n < 0 then dotimes (fun a -> printf "RIGHT\n") (-n);;
+
+let () =
+  let rows = Int.of_string (read_line ()) in
+  let ((px, py), (rx, ry)) = grid_locs rows 0 (-1, -1) (-1, -1) in
+  print_updown (px - rx);
+  print_leftright (px - rx);
+    
