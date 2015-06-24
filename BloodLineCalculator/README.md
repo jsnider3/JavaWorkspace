cc3339c79d74f2f584ea36f4a62dbc239bdd4a61
Joshua Snider <jsnider3@masonlive.gmu.edu>
2015-06-24 01:59:37 -0400

    Tidied up BloodLineCalculator

---
+++ BloodLineCalculator/README.md
@@ -0,0 +1,5 @@
+#### BloodLineCalculator
+
+Old code to calculate how many generations it would take an 
+initial population to grow past a threshold, given a constant
+fertility and a zero mortality rate.
--- BloodLineCalculator/SizeCalculator.java
+++ BloodLineCalculator/SizeCalculator.java
@@ -1,15 +1,18 @@
-
+/** Calculate how many generations it takes a population to grow past
+ * a certain threshold.
+ *
+ */
 public class SizeCalculator {
-	public static void main(String[] args){
-		int FoundingSize=3;
-		int avgDescendentNumber=4;
-		int max=1000000;
-		int total=FoundingSize;
-		int Generations = 0;
-		int cnt = 0;
-		while (cnt<max)
-		{ cnt=total + total*avgDescendentNumber;
-		Generations++;}
-		System.out.println(Generations);
-	}
+  public static void main(String[] args){
+    int start = 3;
+    int avgDescendentNumber = 4;
+    int max = 1000000;
+    int total = start;
+    int Generations = 0;
+    while (total < max) {
+      total += total*avgDescendentNumber;
+      Generations++;
+    }
+    System.out.println(Generations);
+  }
 }
