8c7822b7db962c4ec06fd12a776a953486f23d4e
Joshua Snider <jsnider3@masonlive.gmu.edu>
2015-06-24 02:22:52 -0400

    Style and README

--- DancingMan/DancingMan.java
+++ DancingMan/DancingMan.java
@@ -1,41 +1,39 @@
-
+/** Print a dancing man to the terminal.
+ *
+ */
 public class DancingMan {
 
-	public static void main(String[] args) {
-		int dancepose = 1;
-		int run = 1;
-		while(run == 1){
-			if(dancepose == 3){
-				try {
-					Thread.sleep(125);
-				} catch (InterruptedException e) {
-					
-					e.printStackTrace();
-				}
-				System.out.println("<o>\n| \n/<\n");
-				dancepose = 1;
-			}
-			if(dancepose == 1){
-				try {
-					Thread.sleep(125);
-				} catch (InterruptedException e) {
-					
-					e.printStackTrace();
-				}
-				System.out.println(" o>\n<|  \n >\\\n");
-				dancepose =2;
-				
-			}
-			if(dancepose == 2){
-				try {
-					Thread.sleep(125);
-				} catch (InterruptedException e) {
-					
-					e.printStackTrace();
-				}
-				System.out.println(" o\n<|>\n /<\n");
-				dancepose = 3;
-			}
-		}
-	}
+  public static void main(String[] args) {
+    int dancepose = 1;
+    while (true)
+    {
+      if (dancepose == 3) {
+        try {
+          Thread.sleep(125);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        System.out.println("<o>\n| \n/<\n");
+        dancepose = 1;
+      }
+      if (dancepose == 1) {
+        try {
+          Thread.sleep(125);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        System.out.println(" o>\n<|  \n >\\\n");
+        dancepose = 2;
+      }
+      if (dancepose == 2) {
+        try {
+          Thread.sleep(125);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        System.out.println(" o\n<|>\n /<\n");
+        dancepose = 3;
+      }
+    }
+  }
 }
---
+++ DancingMan/README.md
@@ -0,0 +1,4 @@
+#### DancingMan
+
+Old code, prints out a dancing man on the terminal. I'm not sure
+what the name of the dance is.
