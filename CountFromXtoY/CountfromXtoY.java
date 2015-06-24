d83295bada4e8fa42d7ab8b2da77a8fa9812e9fd
Joshua Snider <jsnider3@masonlive.gmu.edu>
2015-06-24 02:19:05 -0400

    Minor improvments and README

--- CountFromXtoY/CountfromXtoY.java
+++ CountFromXtoY/CountfromXtoY.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
 public class CountfromXtoY {
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		int x, y;
-		String name = "";
-		System.out.println("Enter the higher number: ");
-		x = input.nextInt();
-		
-		System.out.println("Enter the lower number: ");
-		y = input.nextInt();
-				
-		while (x > y){
-			
-			System.out.println(x);
-			x --;
-		}
-	}
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int x, y;
+    String name = "";
+    System.out.println("Enter the first number: ");
+    x = input.nextInt();
+    System.out.println("Enter the second number: ");
+    y = input.nextInt();
+    while (x != y) {
+      System.out.println(x);
+      if (x < y) {
+        x++;
+      } else {
+        x--;
+      }
+    }
+  }
 
 }
---
+++ CountFromXtoY/README.md
@@ -0,0 +1,3 @@
+#### CountFromXtoY
+
+Old code, given an x and y print out [x, y).
