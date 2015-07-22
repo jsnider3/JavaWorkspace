e54cd1ec6500d5d6932ef8ce938c1556de80bc13
Joshua Snider <jsnider3@masonlive.gmu.edu>
2015-07-22 18:31:05 -0400

    More LinkedList methods.

--- Competitive/CPP/LinkedList.cpp
+++ Competitive/CPP/LinkedList.cpp
@@ -4,8 +4,6 @@
     they are not a good example of idiomatic C++.
 
   See HackerRank -> Data Structures -> Linked List.
-  Print elements of a linked list on console 
-  head pointer input could be NULL as well for empty list
   Node is defined as 
   struct Node
   {
@@ -13,6 +11,27 @@
      struct Node *next;
   }
 */
+
+/*
+* Compare two lists.
+* Lists are equal if they have the same length and equal data
+* in each position.
+*/
+bool
+CompareLists(Node *headA, Node* headB)
+{
+  bool equal;
+  if (headA == nullptr && headB != nullptr) {
+    equal = false;
+  } else if (headA != nullptr && headB == nullptr) {
+    equal = false;
+  } else if (headA == nullptr && headB == nullptr) {
+    equal = true;
+  } else {
+    equal = headA->data == headB->data && CompareLists(headA->next, headB->next);
+  }
+  return equal;
+}
 
 /*
 * Remove node at given position.
@@ -26,6 +45,52 @@ Delete(Node *head, int position)
     head->next = Delete(head->next, position - 1);
     return head;
   }
+}
+
+/*
+* Get Nth element from the end in a linked list.
+* Given len(list) > N.
+*/
+int
+GetNode(Node *head, int positionFromTail)
+{
+  auto *iter = head;
+  int len = 0;
+  while (iter != nullptr) {
+    len++;
+    iter = iter->next;
+  }
+  int positionFromHead = len - positionFromTail - 1;
+  iter = head;
+  for (int x = 0; x < positionFromHead; x++) {
+    iter = iter->next;
+  }
+  return iter->data;
+}
+
+/*
+* Check linked list for loops.
+*/
+bool
+HasCycle(Node *head)
+{
+  if (head == nullptr) {
+    return false;
+  }
+  auto *slow = head;
+  auto *fast = head->next;
+  while (fast != nullptr) {
+    if (slow == fast) {
+      return true;
+    } else {
+      slow = slow->next;
+      fast = fast->next;
+      if (fast != nullptr) {
+        fast = fast->next;
+      }
+    }
+  }
+  return false;
 }
 
 /*
@@ -63,6 +128,45 @@ InsertNth(Node *head, int data, int position)
 }
 
 /*
+* Merge two disjoint sorted lists A and B as one linked list.
+*/
+Node*
+MergeLists(Node *headA, Node* headB)
+{
+  if (headA == nullptr) {
+    return headB;
+  } else if (headB == nullptr) {
+    return headA;
+  }
+  Node *min;
+  if (headA->data < headB->data) {
+    min = headA;
+    headA = headA->next;
+  } else {
+    min = headB;
+    headB = headB->next;
+  }
+  Node *iter = min;
+  while (headA != nullptr && headB != nullptr) {
+    if (headA->data < headB->data) {
+      iter->next = headA;
+      iter = iter->next;
+      headA = headA->next;
+    } else {
+      iter->next = headB;
+      iter = iter->next;
+      headB = headB->next;
+    }
+  }
+  if (headA == nullptr) {
+    iter->next = headB;
+  } else {
+    iter->next = headA;
+  }
+  return min;
+}
+
+/*
 * Add to start of linked list.
 */
 Node*
@@ -84,6 +188,41 @@ Print(Node *head)
     std::cout << head->data << std::endl;
     Print(head->next);
   }
+}
+
+/*
+* Removes all duplicate elements from a sorted linked list.
+*/
+Node*
+RemoveDuplicates(Node *head)
+{
+  auto *iter = head;
+  while (iter != nullptr) {
+    auto *next = iter->next;
+    if (next != nullptr && iter->data == next->data) {
+      iter->next = next->next;
+    } else {
+      iter = next;
+    }
+  }
+  return head;
+}
+
+/*
+* Reverse a linked list in-place and return the new head.
+*/
+Node*
+Reverse(Node *head)
+{
+  auto *iter = head;
+  Node *back = nullptr;
+  while (head != nullptr) {
+      auto *temp = head->next;
+      head->next = back;
+      back = head;
+      head = temp;
+  }
+  return back;
 }
 
 /*
