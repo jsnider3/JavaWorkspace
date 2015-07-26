96235a055091552b2fb1aaf0b64434a84b59907c
Joshua Snider <jsnider3@masonlive.gmu.edu>
2015-07-26 08:42:32 -0400

    Better pylint score.

--- Competitive/Python/graph.py
+++ Competitive/Python/graph.py
@@ -1,21 +1,25 @@
-
+''' Represents a directed weighted graph and
+    provides common operations on them.'''
 
 class Graph(object):
   ''' Class for graphs, containing vertices linked by
       weighted edges. '''
-  def __init__(self):
-    self._EdgeMap = {}
-    self._Vertices = set([])
+
+  def __init__(self, directed=True):
+    ''' Graph with no edges or vertices.'''
+    self.edge_map = {}
+    self.vertices = set([])
+    self.directed = directed
 
   def connected(self, source, dest):
     ''' True if there's a path between source and dest.'''
-    if source not in self._EdgeMap or dest not in self._EdgeMap:
+    if source not in self.edge_map or dest not in self.edge_map:
       raise KeyError("Source and dest must be in graph.")
     visits = set()
     queue = [source]
     while queue:
       visit = queue.pop()
-      for neigh in self._EdgeMap[visit]:
+      for neigh in self.edge_map[visit]:
         if neigh not in visits:
           queue.append(neigh)
       visits.add(visit)
@@ -24,134 +28,138 @@ class Graph(object):
   def dijkstra(self, source):
     ''' Perform dijkstra's algorithm
         starting at source. '''
-    tDist = {}
-    tPrev = {}
-    tUnvisited = []
+    dist = {}
+    prev = {}
+    unvisited = []
     #TODO Fixme
-    for vert in self._Vertices:
-      tDist[vert] = float("inf")
-      tPrev[vert] = float("inf")
-      tUnvisited.append(vert)
-    tDist[source] = 0
-    tDist[-2] = float("inf")
-    while tUnvisited:
-      tVisit = min(tUnvisited, key=lambda x: tDist[x])
-      tUnvisited.remove(tVisit)
-      tEdges = self._EdgeMap[tVisit]
-      for (neighbor, leng) in tEdges.items():
-        tWeight = tDist[tVisit] + leng
-        if tWeight < tDist[tNeighbor]:
-          tPrev[neighbor] = tVisit
-          tDist[neighbor] = tWeight
-    return (tDist, tPrev)
+    for vert in self.vertices:
+      dist[vert] = float("inf")
+      prev[vert] = float("inf")
+      unvisited.append(vert)
+    dist[source] = 0
+    while unvisited:
+      visit = min(unvisited, key=lambda x: dist[x])
+      unvisited.remove(visit)
+      edges = self.edge_map[visit]
+      for (neighbor, leng) in edges.items():
+        weight = dist[visit] + leng
+        if weight < dist[neighbor]:
+          prev[neighbor] = visit
+          dist[neighbor] = weight
+    return (dist, prev)
 
   def dump_edges(self):
     ''' Print the edges in the graph. '''
-    print(self._EdgeMap)
+    print(self.edge_map)
+
+  @staticmethod
+  def from_edgelist(edgelist):
+    ''' Make a graph out of a list of edges.
+        Edges have the form of (from, to, weight).'''
+    graph = Graph()
+    for (source, dest, weight) in edgelist:
+      graph.set_edge(source, dest, weight)
+    return graph
 
   @staticmethod
   def from_matrix(matrix):
-    ''' Make a graph out of matrix. '''
+    ''' Make a graph out of a matrix. '''
     graph = Graph()
     rows = len(matrix)
     num_vertices = rows * len(matrix[0])
     func = lambda x, y: rows * y + x
     graph = Graph()
-    for tX in range(rows):
-      for tY in range(rows):
-        vertex = func(tX, tY)
-        left = func(tX - 1, tY)
-        up = func(tX, tY - 1)
-        right = func(tX + 1, tY)
-        down = func(tX, tY + 1)
-        weight = matrix[tY][tX]
-        if tX > 0 and left >= 0:
+    for col in range(rows):
+      for row in range(rows):
+        vertex = func(col, row)
+        left = func(col - 1, row)
+        above = func(col, row - 1)
+        right = func(col + 1, row)
+        down = func(col, row + 1)
+        weight = matrix[row][col]
+        if col > 0 and left >= 0:
           graph.set_edge(left, vertex, weight)
-        if tY > 0 and up >= 0:
-          graph.set_edge(up, vertex, weight)
-        if tX < rows - 1 and right < num_vertices:
+        if row > 0 and above >= 0:
+          graph.set_edge(above, vertex, weight)
+        if col < rows - 1 and right < num_vertices:
           graph.set_edge(right, vertex, weight)
-        if tY < rows - 1 and down < num_vertices:
+        if row < rows - 1 and down < num_vertices:
           graph.set_edge(down, vertex, weight)
     return graph
 
   @staticmethod
-  def from_triangle(aTriangle):
-    ''' Make a graph out of aTriangle. '''
+  def from_triangle(triangle):
+    ''' Make a graph out of a triangle. '''
     count = 0
-    rows = len(aTriangle)
+    rows = len(triangle)
     graph = Graph()
     for row in range(rows):
-      tColumns = len(aTriangle[row])
+      columns = len(triangle[row])
       if row != rows - 1:
-        next_row = aTriangle[row + 1]
-        for tX in range(tColumns):
-          vertex = count + tX
-          left_child = vertex + tColumns
-          graph.set_edge(vertex, left_child, next_row[tX])
-          graph.set_edge(vertex, left_child + 1, next_row[tX + 1])
+        next_row = triangle[row + 1]
+        for col in range(columns):
+          vertex = count + col
+          left_child = vertex + columns
+          graph.set_edge(vertex, left_child, next_row[col])
+          graph.set_edge(vertex, left_child + 1, next_row[col + 1])
       else:
-        for tX in range(tColumns):
-          vertex = count + tX
+        for col in range(columns):
+          vertex = count + col
           graph.set_edge(vertex, -1, 0)
-      count += tColumns
+      count += columns
     return graph
 
   def longest_dag_path(self, source, dest):
     ''' Given that self is a dag, find the longest path
         between source and dest. '''
-    tDist = {}
-    tPrev = {}
-    tUnvisited = []
-    for tKey in self._Vertices:
-      tDist[tKey] = - float("inf")
-      #tPrev[tKey] = - float("inf")
-      tUnvisited.append(tKey)
-    tDist[source] = 0
-    while tUnvisited:
-      tVisit = max(tUnvisited, key=lambda x: tDist[x])
-      tUnvisited.remove(tVisit)
-      tEdges = self._EdgeMap[tVisit]
-      for entry in tEdges.items():
-        (tNeighbor, tLeng) = entry
-        weight = -(tDist[tVisit] + tLeng)
-        if weight > tDist[tNeighbor]:
-          tPrev[tNeighbor] = tVisit
-          tDist[tNeighbor] = weight
-    tTree = tPrev
-    tPath = []
-    tPrev = dest
-    while tPrev != source:
-      tPath.append(tPrev)
-      tTemp = tPrev
-      tPrev = tTree[tPrev]
-    tPath.append(tPrev)
-    tPath.reverse()
-    return tPath
+    dist = {}
+    prev = {}
+    unvisited = []
+    for key in self.vertices:
+      dist[key] = - float("inf")
+      unvisited.append(key)
+    dist[source] = 0
+    while unvisited:
+      visit = max(unvisited, key=lambda x: dist[x])
+      unvisited.remove(visit)
+      edges = self.edge_map[visit]
+      for entry in edges.items():
+        (neighbor, leng) = entry
+        weight = -(dist[visit] + leng)
+        if weight > dist[neighbor]:
+          prev[neighbor] = visit
+          dist[neighbor] = weight
+    tree = prev
+    path = []
+    prev = dest
+    while prev != source:
+      path.append(prev)
+      prev = tree[prev]
+    path.append(prev)
+    path.reverse()
+    return path
 
-  def set_edge(self, aX, aY, weight=1):
-    ''' Set the weight of aX -> aY to weight. '''
-    self._Vertices.add(aX)
-    self._Vertices.add(aY)
-    if aX in self._EdgeMap:
-      self._EdgeMap[aX][aY] = int(weight)
-    else:
-      self._EdgeMap[aX] = {aY : int(weight)}
-    if not aY in self._EdgeMap:
-      self._EdgeMap[aY] = {}
+  def set_edge(self, source, dest, weight=1):
+    ''' Set the weight of source -> dest to weight. '''
+    self.vertices.add(source)
+    self.vertices.add(dest)
+    if source not in self.edge_map:
+      self.edge_map[source] = {}
+    self.edge_map[source][dest] = int(weight)
+    if not dest in self.edge_map:
+      self.edge_map[dest] = {}
+    if not self.directed:
+      self.edge_map[dest][source] = int(weight)
 
   def shortest_path(self, source, dest):
     ''' Return the nodes along the shortest path from
-        source to dest. '''
-    (tWeight, tTree) = self.dijkstra(source)
-    tPath = []
-    tPrev = dest
-    tDist = tWeight[tPrev]
-    while tPrev != source:
-      tPath.append(tPrev)
-      tTemp = tPrev
-      tPrev = tTree[tPrev]
-      tDist = tWeight[tTemp]
-    tPath.append(tPrev)
-    tPath.reverse()
-    return tPath
+        source to dest. Includes source and dest.'''
+    (_, tree) = self.dijkstra(source)
+    path = []
+    prev = dest
+    while prev != source:
+      path.append(prev)
+      prev = tree[prev]
+    path.append(prev)
+    path.reverse()
+    return path
