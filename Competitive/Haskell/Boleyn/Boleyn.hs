a6b5a3472f000ca18e86d81c4cd3378be349450a
Joshua Snider <jsnider3@masonlive.gmu.edu>
2016-04-06 02:30:17 -0400

    Alphabetized methods and medium refactoring.

--- Competitive/Haskell/Boleyn/Boleyn.hs
+++ Competitive/Haskell/Boleyn/Boleyn.hs
@@ -11,22 +11,29 @@ import qualified Data.Set as Set
   structure.
   On the plus side, the results are 100% correct.
 -}
-createHierarchy :: [String] -> Map.Map Int (Set.Set Int)
-createHierarchy links = 
-  let supes = map makeIntList (map words links)
-      dests = map (\x -> x !! 1) supes in
-    addLeaves (edgesToTree(zip (map head supes)dests)) (map head supes)
+addLeaves :: (Ord a) => (Map.Map a (Set.Set a)) -> [a] -> (Map.Map a (Set.Set a))
+addLeaves a [] = a
+addLeaves a (n:nodes) = addLeaves (Map.insertWith Set.union n Set.empty a) nodes
 
-isleaf :: Map.Map Int (Set.Set Int) -> Int -> Bool
+createHierarchy :: [(Int, Int)] -> Map.Map Int (Set.Set Int)
+createHierarchy edges = 
+    addLeaves (edgesToTree(zip (map fst edges) (map snd edges))) (map fst edges)
+
+{-
+  Represent a graph as an adjacency list given a list of (src, dest) pairs.
+-}
+edgesToTree :: (Ord a) => [(a, a)] -> (Map.Map a (Set.Set a))
+edgesToTree [] = Map.empty
+edgesToTree ((a, b):rest) = Map.insertWith (Set.union) b (Set.singleton a) (edgesToTree rest)
+
+isleaf :: (Ord a, Eq b) => Map.Map a (Set.Set b) -> a -> Bool
 isleaf orgChart emp = fromJust (Map.lookup emp orgChart) == Set.empty
 
-toposort :: Map.Map Int (Set.Set Int) -> [Int]
-toposort orgChart
-  | Map.null orgChart = []
-  | otherwise = let leaves = filter (isleaf orgChart) (Map.keys orgChart)
-                    prunedOrg = Map.filterWithKey (\k _ -> not (isleaf orgChart k)) orgChart 
-                    prunedOrg2 = Map.map (\v -> Set.difference v (Set.fromList leaves)) prunedOrg in
-    leaves ++ (toposort prunedOrg2) 
+{-
+  Read a list of strings to a list of ints.
+-}
+makeIntList :: [String] -> [Int]
+makeIntList = map read
 
 makeSalmap :: Map.Map Int (Set.Set Int) -> [Int] -> (Int -> Int) -> Map.Map Int [Int] -> Map.Map Int [Int]
 makeSalmap _ [] _ s = s
@@ -36,11 +43,17 @@ makeSalmap orgChart (emp:emps) sals salmap =
       merged = merge2 sals (directs:indirects) in
     makeSalmap orgChart emps sals (Map.insert emp merged salmap)
 
+{-
+  Merge n sorted sets into one given the key function.
+-}
 merge2 _ [] = []
 merge2 _ [a] = a
 merge2 key ls = merge key (merge2 key (take (length ls `quot` 2) ls))
                           (merge2 key (drop (length ls `quot` 2) ls))
 
+{-
+  Merge two sorted sets into one given the key function.
+-}
 merge _ [] b = b
 merge _ a [] = a
 merge key (a:as) (b:bs) = case (a == b, key a < key b) of
@@ -48,31 +61,22 @@ merge key (a:as) (b:bs) = case (a == b, key a < key b) of
   (_, True) -> a : merge key as (b:bs)
   (_, False) -> b : merge key (a:as) bs
 
+readEdges :: [String] -> [(Int, Int)]
+readEdges lines = map (tuplify2 . makeIntList . words) lines
+
 {-
-  Get the subtree in tree anchored at the given root.
+  Sort a list with the given value function.
 -}
-descendents :: Array Int [Int] -> Int -> [Int]
-descendents tree root = case (tree ! root ) of
-                          [] -> []
-                          a -> a ++ concat(map (descendents tree) a)
-
-sortedDescendents :: Array Int [Int] -> Int -> Array Int Int -> [Int]
-sortedDescendents tree root sals = sortOn (sals!) (descendents tree root)
-
 sortOn :: (Ord a, Ord b) => (a -> b) -> [a] -> [a]
 sortOn f ls = map snd (sort (map (\i -> (f i, i)) ls))
 
-addLeaves :: (Ord a) => (Map.Map a (Set.Set a)) -> [a] -> (Map.Map a (Set.Set a))
-addLeaves a [] = a
-addLeaves a (n:nodes) = addLeaves (Map.insertWith Set.union n Set.empty a) nodes
-
-{-
-  Represent a graph as an adjacency list given a list of (src, dest) pairs.
-  TODO Add leaves to tree.
--}
-edgesToTree :: (Ord a) => [(a, a)] -> (Map.Map a (Set.Set a))
-edgesToTree [] = Map.empty
-edgesToTree ((a, b):rest) = Map.insertWith (Set.union) b (Set.singleton a) (edgesToTree rest)
+toposort :: Map.Map Int (Set.Set Int) -> [Int]
+toposort orgChart
+  | Map.null orgChart = []
+  | otherwise = let leaves = filter (isleaf orgChart) (Map.keys orgChart)
+                    prunedOrg = Map.filterWithKey (\k _ -> not (isleaf orgChart k)) orgChart 
+                    prunedOrg2 = Map.map (\v -> Set.difference v (Set.fromList leaves)) prunedOrg in
+    leaves ++ (toposort prunedOrg2) 
 
 {-
   Convert the adjacency list to tree form.
@@ -80,16 +84,12 @@ edgesToTree ((a, b):rest) = Map.insertWith (Set.union) b (Set.singleton a) (edge
 treeToArray :: Int -> (Map.Map Int [Int]) -> (Array Int [Int])
 treeToArray n a = array (1, n)(Map.assocs a ++ zip (filter (\x -> Map.notMember x a)[1..n])(repeat []))
 
+tuplify2 [a,b] = (a, b)
+
 parseSalaries :: String -> Array Int Int
 parseSalaries line = 
   let dat = makeIntList (words line) in
     array (1, length dat)(zip [1..] dat)
-
-{-
-  Read a list of strings to a list of ints.
--}
-makeIntList :: [String] -> [Int]
-makeIntList = map read
 
 processQueries :: Map.Map Int [Int] -> [[Int]] -> [Int]
 processQueries sortdecs [[a, b]] = [(fromJust(Map.lookup a sortdecs)) !! (b - 1)]
@@ -97,16 +97,16 @@ processQueries sortdecs ([a, b]:[x,y]:rest) =
   let hd = (fromJust(Map.lookup a sortdecs)) !! (b - 1) in
     hd:(processQueries sortdecs ([hd + x, y]:rest))
 
-printQueries :: Map.Map Int (Set.Set Int) -> Array Int Int -> Int -> IO ()
-printQueries tree sals n = do
-  input <- replicateM n getLine
-  let quers = map makeIntList (map words input) in do
-    mapM_ print (processQueries (makeSalmap tree (toposort tree) (sals!) Map.empty) quers)
-
 main :: IO ()
 main = do
   nums <- getLine
   links <- replicateM (read(head (words nums)) - 1) getLine
   sals <- getLine
-  printQueries (createHierarchy links) (parseSalaries sals) (read((words nums) !! 1)::Int)
-
+  let edges = readEdges links
+      tree = createHierarchy edges
+      num_quers = read((words nums) !! 1)
+      salaries = parseSalaries sals in do
+    input <- replicateM num_quers getLine
+    let quers = map (makeIntList . words) input
+        salmap = makeSalmap tree (toposort tree) (salaries!) Map.empty in do
+      mapM_ print (processQueries salmap quers)
