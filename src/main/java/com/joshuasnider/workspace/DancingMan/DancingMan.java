88783522ef063997c3f779d474859a54edb88120
Joshua Snider <josh@joshuasnider.com>
2016-05-05 03:08:56 -0400

    Refactored DancingMan heavily.

--- src/main/java/com/joshuasnider/workspace/DancingMan/DancingMan.java
+++ src/main/java/com/joshuasnider/workspace/DancingMan/DancingMan.java
@@ -1,39 +1,28 @@
 /** Print a dancing man to the terminal.
- *
+ *  One of my first ever programs. Has since been refactored.
  */
+
+package com.joshuasnider.workspace.dancingman;
+
 public class DancingMan {
 
   public static void main(String[] args) {
-    int dancepose = 1;
-    while (true)
-    {
-      if (dancepose == 3) {
-        try {
-          Thread.sleep(125);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-        System.out.println("<o>\n| \n/<\n");
-        dancepose = 1;
-      }
-      if (dancepose == 1) {
-        try {
-          Thread.sleep(125);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-        System.out.println(" o>\n<|  \n >\\\n");
-        dancepose = 2;
-      }
-      if (dancepose == 2) {
-        try {
-          Thread.sleep(125);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-        System.out.println(" o\n<|>\n /<\n");
-        dancepose = 3;
-      }
+    int dancepose = 0;
+    String[] poses = {"<o>\n| \n/<\n",
+                      " o>\n<|  \n >\\\n",
+                      " o\n<|>\n /<\n"};
+    while (true) {
+      sleepOneFrame();
+      System.out.println(poses[dancepose]);
+      dancepose = (dancepose + 1) % 3;
+    }
+  }
+
+  public static void sleepOneFrame() {
+    try {
+      Thread.sleep(125);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
   }
 }
