dbf4f1db0fa22804bb26f739fa5590b19a7912ce
Josh Snider <Joshua Snider>
2018-04-18 14:53:58 -0400

    Started AutoFish.

---
+++ AutoFish/README.md
@@ -0,0 +1,9 @@
+# AutoFish
+Essentially, I want to automate a task in Secrets of Grindea,
+specifically the fishing minigame.
+
+## Dependencies
+go, which on apt systems is in the package gccgo-go.
+
+## How to run
+go run main.go
---
+++ AutoFish/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+// main starts the fishing process.
+func main() {
+	// 1. Ensure that Secrets of Grindea is running in the foreground.
+	// 2. Simulate a 'Z' keypress.
+	// 3. Wait until we have a fish on the hook.
+	// 4. Press 'Z' to start reeling in the fish.
+	// 5. Press the left and right keys to play the minigame.
+	// 6. Repeat steps 2 through 5 until cancelled.
+	fmt.Printf("AutoFish is not yet finished.\n")
+}
