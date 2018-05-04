8a0979556f4e7a6e561fec70aeb397050b6984aa
Joshua Snider <josh@joshuasnider.com>
2018-05-04 02:58:53 -0400

    Got machinima.py working on Windows 10.

--- Utilities/machinima.py
+++ Utilities/machinima.py
@@ -1,6 +1,10 @@
 import os
 import youtube_dl
 
+def cmd(sh_cmd):
+  print(sh_cmd)
+  os.system(sh_cmd)
+
 def set_artist():
   #TODO
   videos = os.listdir('Lyric Videos')
@@ -8,7 +12,8 @@ def set_artist():
     print(os.path.join(os.getcwd(), 'Lyric Videos', video))
 
 def convert_all_to_mp3():
-  os.mkdir('Output')
+  if not os.path.exists('Output'):
+    os.mkdir('Output')
   videos = os.listdir('Lyric Videos')
   for video in videos:
     os.system('ffmpeg -i "{0}" -f mp3 "Output/{1}.mp3"'.format(
@@ -16,12 +21,14 @@ def convert_all_to_mp3():
 
 def get_machinima():
   print("Get list of JT Machinima videos.")
-  os.system(
-    "youtube-dl --extract-audio -o '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' {0}".format(
+  cmd(
+    'youtube-dl --audio-format mp3 -x -o {0} {1}'.format(
+    '"%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s"',
     "https://www.youtube.com/playlist?list=PLjO04ZlrzPlh90yQok1nZSn4VX0pCCc64"))
   #videos = []
   #for video in videos:
   #  download_youtube(video)
 
 if __name__ == "__main__":
-  convert_all_to_mp3()
+  get_machinima()
+  #convert_all_to_mp3()
